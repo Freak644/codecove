6ebf123f8243e707b30ee0ca402661889fd4edc7
Freak644 <hoodafreak@gmail.com>
2025-10-28 22:26:40 +0530

    will be start post work tomoro

--- client/src/components/myApp.jsx
+++ client/src/components/myApp.jsx
@@ -97,7 +97,7 @@ export default function MyApp() {
            {isCropping && <CropperEL prevImg={fileURL} />}
            {(isLogin && !isChecking) && (<div className='loginContainer flex items-center content-center h-[100vh] w-[100vw]'>{<LoginEL/>}</div>)}
             {(!isLogin || isChecking) && (<Routes>
-                <Route path='/' element={<div className='routeContainer bg-amber-300 my-scroll flex items-center content-center'></div>} />
+                <Route path='/' element={<div className='routeContainer my-scroll flex items-center content-center'></div>} />
                 <Route path='/CheckInfo/:session_id' element={<div className='my-scroll flex items-center justify-center h-screen w-screen'>{<CheckInfo/>}</div>} />
                 <Route path='*' element={<NotFound/>} />
             </Routes>)}

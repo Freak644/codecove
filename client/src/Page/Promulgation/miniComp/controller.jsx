3ac701adbaf962beae103e76de9b76982a70c86c
Freak644 <hoodafreak@gmail.com>
2025-11-24 09:53:48 +0530

    complete UI for post / Compnent animation is now more stable

--- client/src/Page/Promulgation/miniComp/controller.jsx
+++ client/src/Page/Promulgation/miniComp/controller.jsx
@@ -5,19 +5,47 @@ export default function UploadController() {
 
     return(
         <div className="underTaker">
-            <div className="baseCon">
+            <div className="ControllerbaseCon flex items-center md:flex-row flex-col p-4 h-full">
                 <div className="leftDiv">
                     <div className="selectionDiv">
                         <strong>Post Visibility:</strong>
+                        <div className="shadOption">
+                            <p>Public</p>
+                            <p>Privat</p>
+                        </div>
+                    </div>
+                    <div className="selectionDiv">
+                        <strong>Show Like & Comment Count:</strong>
+                        <div className="shadOption">
+                            <p>On</p>
+                            <p>Off</p>
+                        </div>
+                    </div>
+                    <div className="selectionDiv">
+                        <strong>can comment ?:</strong>
+                        <div className="shadOption">
+                            <p>On</p>
+                            <p>Off</p>
+                        </div>
+                    </div>
+                    <div className="selectionDiv">
+                        <strong>Who can save your Post:</strong>
                         <select name="" id="post">
-                            <option value={true}>Public</option>
-                            <option value={false}>Private</option>
+                            <option value={true}>Everyone</option>
+                            <option value={"follower"}>Follower only</option>
+                            <option value="false">NoBody</option>
                         </select>
                     </div>
                 </div>
 
                 <div className="rightDiv">
-
+                    <h2 flex>Block Comments:-</h2>
+                    <div className="shadOption">
+                        <p>Abuse</p>
+                        <p>Spam</p>
+                        <p>Link</p>
+                        <p>Violence</p>
+                    </div>
                 </div>
             </div>
         </div>

84963c280eaa896e74d57652392771920d80aa4c
Freak644 <hoodafreak@gmail.com>
2025-12-22 22:11:51 +0530

    little progress

--- client/src/Page/HomeComponent/maximizeThings/baseContainer.jsx
+++ client/src/Page/HomeComponent/maximizeThings/baseContainer.jsx
@@ -63,7 +63,7 @@ export default function MaximizeContainer() {
                     </button>
                 </div>
                 <div ref={containerRef} className="h-9/10 w-5/6 rounded-lg p-2 flex items-center justify-center flex-wrap border border-white bg-black/50 backdrop-blur-lg">
-                    <div className="flex-2 flex items-center justify-center border border-green-500 h-full relative transition-all duration-200">
+                    <div className="flex-1 flex items-center justify-center border border-green-500 h-full relative transition-all duration-200">
                     <i className={`bx bx${isFull ? "-exit-" : "-"}fullscreen absolute bottom-4 right-5 z-20 text-skin-ptext text-2xl bg-black p-2 cursor-pointer rounded-full`} onClick={()=>setFull(prev=>!prev)}></i>
                     <ImageSlider imgArray={crntPost.images_url || []} />
                     </div>

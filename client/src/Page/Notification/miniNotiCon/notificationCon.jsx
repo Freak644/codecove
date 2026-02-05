c902b804de652838a6b3639260f6d5b9e72bf3e2
Freak644 <hoodafreak@gmail.com>
2026-02-05 20:57:23 +0530

    build dynamic notification with toggle mini the zustan that handle charts also

--- client/src/Page/Notification/miniNotiCon/notificationCon.jsx
+++ client/src/Page/Notification/miniNotiCon/notificationCon.jsx
@@ -1,15 +1,24 @@
+import { toggleSlider } from "../../../lib/tabToggle"
+
 export default function NotificaitonMini() {
-    
+    const {toggleMiniTab} = toggleSlider();
     return(
-        <div className="underTaker">
-            <div className="NotificationHeader h-1/10 w-full flex
-            items-center justify-between text-skin-text">
+        <div className="underTaker flex-wrap bg-skin-text/5 rounded-lg backdrop-blur-lg">
+            <div className="NotificationHeader h-2/10 w-full flex
+            items-start flex-wrap gap-3 text-skin-text">
+                <div className="flex justify-end items-center gap-2.5 w-full">
+                    <p className="text-md text-skin-text/80 self-start">Notification</p>
+                    <p className="text-sm text-blue-400 cursor-pointer hover:text-blue-500">Mark as read</p>
+                    <i onClick={()=>toggleMiniTab("charts")} className="bx bx-exit p-1 rounded-lg text-lg border-2 border-gray-400/30 cursor-pointer"></i>
+                </div>
                 <p>All</p>
-                <p>Activity</p>
                 <p>Update</p>
                 <p>System</p>
+                <p>Read</p>
             </div>
+            <div className="notiContainer h-8/10 w-full">
 
+            </div>
         </div>
     )
 }

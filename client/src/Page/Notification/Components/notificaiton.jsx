7e20887d55b8a5625b88ba809a51e2a1bc94fa02
Freak644 <hoodafreak@gmail.com>
2026-02-21 23:29:05 +0530

    Notification: create UI for Notificaiton

--- client/src/Page/Notification/Components/notificaiton.jsx
+++ client/src/Page/Notification/Components/notificaiton.jsx
@@ -1,24 +1,38 @@
 
-let testObj = {
-    logo_img:"https://res.cloudinary.com/dcq0dge7f/image/upload/v1753269094/ztvkc6flklojhbbgnvc1.jpg",
-    message:"New Notification",
-    time:"today",
-    cat:"Update"
-
-}
 export default function NotificationMgmt({data,cetogry}) {
     //let {logo_img,recipient_id,actor_id,type,entity_id,entity_type, message, is_read} = data;
     return(
-        <div className="underTaker my-scroll">
-            <div className="notificationContainer h-auto w-full ">
-                {
-                    [1,2,3,4,5].map(crntNoti=>(
-                        <div className="notification">
-                            
-                        </div>
-                    ))
-                }
+        <div className="underTaker">
+            <div className="w-full flex flex-col gap-3">
+                {[...Array(12)].map((_, index) => (
+                <div
+                    key={index}
+                    className="w-full h-20 flex items-center gap-3 p-3
+                           rounded-lg
+                            text-skin-text cursor-pointer
+                            hover:bg-amber-500/10 transition"
+                >
+                    {/* Avatar */}
+                    <div className="h-full aspect-square overflow-hidden rounded-full">
+                        <img
+                            className="h-10 w-10 object-cover"
+                            src="https://res.cloudinary.com/dcq0dge7f/image/upload/v1771254694/mjyanart-20250729-0023_uwr6eg.jpg"
+                            alt="user"
+                        />
+                    </div>
+
+                    {/* Message */}
+                    <div className="flex flex-col justify-center overflow-hidden">
+                        <p className="text-sm md:text-[14px] font-semibold truncate">
+                            Freak_3221 commented on your post
+                        </p>
+                        <p className="text-xs md:text-[14px] text-skin-ptext">
+                            A boy how is sitting in a dark cornner of a dark room waiting for the moringing 
+                        </p>
+                    </div>
+                </div>
+                ))}
             </div>
-        </div>
+            </div>
     )
 }
--- client/src/Page/Notification/Components/notificationFeed.jsx
+++ client/src/Page/Notification/Components/notificationFeed.jsx
@@ -1,9 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { toggleSlider } from "../../../lib/tabToggle"
 import { notification } from "../../../utils/notificationSender";
+import NotificationMgmt from "./notificaiton";
 
 export default function NotificaitonMini() {
     const {toggleMiniTab} = toggleSlider();
+    const [notiTab,setTab] = useState("All");
 
     useEffect(()=>{
         
@@ -13,21 +15,21 @@ export default function NotificaitonMini() {
         notification("Test 2/.3","Testing",true,"http://localhost:3221/post/kBO61tMtRgdpofmEHqmd4")
     }
     return(
-        <div className="underTaker flex-wrap bg-skin-text/2 rounded-lg backdrop-blur-lg">
-            <div className="NotificationHeader h-2/10 w-full flex
+        <div className="underTaker flex-wrap bg-skin-text/1 rounded-lg backdrop-blur-lg">
+            <div className="NotificationHeader h-2/14 w-full flex 
             items-start flex-wrap gap-3 text-skin-text justify-center">
                 <div className="flex justify-end items-center gap-2.5 w-full">
                     <p onClick={showTestNotification} className="text-lg flex gap-2 text-skin-text/80 items-center"><i className="bx bxs-bell-ring"></i></p>
                     <p className="text-sm text-blue-400 cursor-pointer hover:text-blue-500 ml-auto">Mark as read</p>
                     <i onClick={()=>toggleMiniTab("charts")} className="bx bx-exit p-1 rounded-lg text-lg border-2 border-gray-400/30 cursor-pointer"></i>
                 </div>
-                <p>All</p>
-                <p>Update</p>
-                <p>System</p>
-                <p>Read</p>
+                <p onClick={()=>setTab("All")} className={notiTab === "All" && "activeNoti"}>All</p>
+                <p onClick={()=>setTab("Update")} className={notiTab === "Update" && "activeNoti"}>Update</p>
+                <p onClick={()=>setTab("Sys")} className={notiTab === "Sys" && "activeNoti"}>System</p>
+                <p onClick={()=>setTab("Read")} className={notiTab === "Read" && "activeNoti"}>Read</p>
             </div>
-            <div className="notiContainer h-8/10 w-full">
-                
+            <div className="h-12/14 w-full my-scroll pt-50">
+                <NotificationMgmt/>
             </div>
         </div>
     )

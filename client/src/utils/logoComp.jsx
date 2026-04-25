621d4de0e342e2e136a2fc6ddfc6ddca515c5570
Gabimaru <hoodafreak@gmail.com>
2026-04-25 13:50:27 +0530

    generate the front Page Loader

--- client/src/utils/logoComp.jsx
+++ client/src/utils/logoComp.jsx
@@ -8,12 +8,12 @@ export default function LogoCom({CustomclassName}) {
             bg-linear-to-tr ${gradColor}
             bg-clip-text text-transparent
             `}></i>
-            <div className="nameHoler flex items-center flex-row">
+            <div className="nameHoler gap-1 flex items-center flex-row">
                 <h2 className='font-bold text-3xl transition-all duration-300 ease-in-out
-                bg-linear-to-b from-blue-300 to-blue-600 bg-clip-text text-transparent'>Null</h2>
+                bg-linear-to-b from-blue-300 to-blue-600 bg-clip-text text-transparent'>Echo</h2>
                 <h2 className={`font-bold text-3xl transition-all duration-500 ease-in-out 
                 bg-linear-to-b from-yellow-300 from-40% via-pink-500 to-purple-500
-                bg-clip-text text-transparent`}>Vain</h2>
+                bg-clip-text text-transparent`}>Nexy</h2>
             </div>
         </div>
     )
